a0bc8aa6afbae47cc132ccc6cbfb12b793e8272e
Oleg Shipulin <olegshipu95@gmail.com>
2024-01-07 09:21:00 +0300

    write ddl

--- delete_kursach.sql
+++ delete_kursach.sql
@@ -4,13 +4,15 @@ drop table if exists elected_ability;
 drop table if exists ability;
 drop table if exists elected;
 drop table if exists instrument;
+drop table if exists instrument_status;
 drop table if exists runes;
 drop table if exists human_action;
 drop table if exists action;
-drop table if exists abbys;
 drop table if exists outsider;
+drop table if exists abbys;
 drop table if exists abbey;
 drop table if exists abbey_member;
+drop table if exists abbey_rank_status;
 drop table if exists human;
 drop table if exists human_status;
 
--- kursach.sql
+++ kursach.sql
@@ -1,101 +1,185 @@
-CREATE TABLE if not exists altar
+CREATE TABLE altar
 (
-    id        SERIAL PRIMARY KEY NOT NULL,
-    author_id INTEGER            NOT NULL
+    id        INTEGER NOT NULL,
+    author_id INTEGER NOT NULL
 );
+
+ALTER TABLE
+    altar
+    ADD PRIMARY KEY (id);
+
+CREATE TABLE human_status
+(
+    id     BIGINT       NOT NULL,
+    status VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE
+    human_status
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE Action
 (
-    id              SERIAL PRIMARY KEY             NOT NULL,
-    rating_delta    BIGINT                         NOT NULL CHECK ( rating_delta >= 0 and rating_delta <= 100 ),
+    id              BIGINT                         NOT NULL,
+    rating_delta    BIGINT                         NOT NULL,
     expire_duration TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
+
+ALTER TABLE
+    Action
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE abbys
 (
-    id        SERIAL PRIMARY KEY NOT NULL,
-    mana      BIGINT             NOT NULL,
-    outsiders BIGINT             NOT NULL
+    id   BIGINT NOT NULL,
+    mana BIGINT NOT NULL
 );
+
+ALTER TABLE
+    abbys
+    ADD PRIMARY KEY (id);
+
+create table instrument_status
+(
+    id          BIGINT      NOT NULL,
+    description VARCHAR(40) NOT NULL
+);
+
+ALTER TABLE
+    instrument_status
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE instrument
 (
-    id          SERIAL PRIMARY KEY NOT NULL,
-    description VARCHAR(40)        NOT NULL,
-    status      BIGINT             NOT NULL
+    id          BIGINT                                   NOT NULL,
+    description VARCHAR(40)                              NOT NULL,
+    status      BIGINT references instrument_status (id) NOT NULL
 );
+
+ALTER TABLE
+    instrument
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE elected
 (
-    id          SERIAL PRIMARY KEY NOT NULL,
-    human_id    BIGINT             NOT NULL,
-    runes       BIGINT             NOT NULL,
-    outsider_id BIGINT             NOT NULL
+    id          BIGINT NOT NULL,
+    human_id    BIGINT NOT NULL,
+    runes       BIGINT NOT NULL,
+    outsider_id BIGINT NOT NULL
 );
+
+ALTER TABLE
+    elected
+    ADD PRIMARY KEY (id);
+
+create table abbey_rank_status
+(
+    id          BIGINT      NOT NULL,
+    description VARCHAR(40) NOT NULL
+);
+
+ALTER TABLE
+    abbey_rank_status
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE abbey_member
 (
-    id       SERIAL PRIMARY KEY NOT NULL,
-    human_id BIGINT             NOT NULL,
-    rank     BIGINT             NOT NULL
+    id       BIGINT NOT NULL,
+    human_id BIGINT NOT NULL,
+    rank     BIGINT NOT NULL
 );
+
+ALTER TABLE
+    abbey_member
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE ability
 (
-    "id"          SERIAL PRIMARY KEY NOT NULL,
-    "description" VARCHAR(255)       NOT NULL,
-    "cost"        BIGINT             NOT NULL
+    id          BIGINT       NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    cost        BIGINT       NOT NULL
 );
+
+ALTER TABLE
+    ability
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE runes
 (
-    id             SERIAL PRIMARY KEY NOT NULL,
-    contained_mana BIGINT             NOT NULL
+    id             INTEGER NOT NULL,
+    contained_mana BIGINT  NOT NULL
 );
+
+ALTER TABLE
+    runes
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE sacrifice
 (
-    id               SERIAL PRIMARY KEY             NOT NULL,
+    id               BIGINT                         NOT NULL,
     data             TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
     created_outsider BIGINT                         NOT NULL,
     instrument_id    BIGINT                         NOT NULL,
-    abbey            BIGINT                         NOT NULL
+    abbey_member     BIGINT                         NOT NULL
 );
+
+ALTER TABLE
+    sacrifice
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE elected_ability
 (
     elected_id BIGINT NOT NULL,
     ability    BIGINT NOT NULL
 );
+
 CREATE TABLE Human_action
 (
-    id        SERIAL PRIMARY KEY             NOT NULL,
+    id        BIGINT                         NOT NULL,
     human_id  BIGINT                         NOT NULL,
     action_id BIGINT                         NOT NULL,
     date      TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
 );
 
+ALTER TABLE
+    Human_action
+    ADD PRIMARY KEY (id);
+
 CREATE TABLE outsider
 (
-    id       SERIAL PRIMARY KEY NOT NULL,
-    status   BIGINT             NOT NULL,
-    human_id BIGINT             NOT NULL
+    id       BIGINT NOT NULL,
+    status   BIGINT NOT NULL,
+    human_id BIGINT NOT NULL,
+    abbys_id BIGINT NOT NULL
 );
+
+ALTER TABLE
+    outsider
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE abbey
 (
-    id      SERIAL PRIMARY KEY NOT NULL,
-    name    BIGINT             NOT NULL,
-    members BIGINT             NOT NULL
+    id      BIGINT NOT NULL,
+    name    BIGINT NOT NULL,
+    members BIGINT NOT NULL
 );
 
-create table if not exists human_status
-(
-    id     SERIAL PRIMARY KEY NOT NULL,
-    status varchar(40)        not null
-);
+ALTER TABLE
+    abbey
+    ADD PRIMARY KEY (id);
 
 CREATE TABLE human
 (
-    id        SERIAL PRIMARY KEY                   NOT NULL,
-    name      VARCHAR(255)                         NOT NULL,
-    status_id INTEGER references human_status (id) NOT NULL
+    id            BIGINT                         NOT NULL,
+    name          VARCHAR(255)                   NOT NULL,
+    date_of_birth TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL,
+    status        BIGINT                         NOT NULL
 );
+
 ALTER TABLE
-    sacrifice
-    ADD CONSTRAINT sacrifice_abbey_foreign FOREIGN KEY (abbey) REFERENCES abbey (id);
+    human
+    ADD PRIMARY KEY (id);
+
 ALTER TABLE
     elected_ability
     ADD CONSTRAINT elected_ability_elected_id_foreign FOREIGN KEY (elected_id) REFERENCES elected (id);
@@ -112,8 +196,8 @@ ALTER TABLE
     elected
     ADD CONSTRAINT elected_runes_foreign FOREIGN KEY (runes) REFERENCES runes (id);
 ALTER TABLE
-    elected
-    ADD CONSTRAINT elected_outsider_id_foreign FOREIGN KEY (outsider_id) REFERENCES outsider (id);
+    human
+    ADD CONSTRAINT human_status_foreign FOREIGN KEY (status) REFERENCES human_status (id);
 ALTER TABLE
     outsider
     ADD CONSTRAINT outsider_human_id_foreign FOREIGN KEY (human_id) REFERENCES human (id);
@@ -127,14 +211,20 @@ ALTER TABLE
     sacrifice
     ADD CONSTRAINT sacrifice_instrument_id_foreign FOREIGN KEY (instrument_id) REFERENCES instrument (id);
 ALTER TABLE
-    abbys
-    ADD CONSTRAINT abbys_outsiders_foreign FOREIGN KEY (outsiders) REFERENCES outsider (id);
+    sacrifice
+    ADD CONSTRAINT sacrifice_abbey_member_foreign FOREIGN KEY (abbey_member) REFERENCES abbey_member (id);
 ALTER TABLE
     abbey_member
     ADD CONSTRAINT abbey_member_human_id_foreign FOREIGN KEY (human_id) REFERENCES human (id);
 ALTER TABLE
+    outsider
+    ADD CONSTRAINT outsider_abbys_id_foreign FOREIGN KEY (abbys_id) REFERENCES abbys (id);
+ALTER TABLE
     altar
     ADD CONSTRAINT altar_author_id_foreign FOREIGN KEY (author_id) REFERENCES human (id);
+ALTER TABLE
+    elected
+    ADD CONSTRAINT elected_outsider_id_foreign FOREIGN KEY (outsider_id) REFERENCES outsider (id);
 ALTER TABLE
     Human_action
     ADD CONSTRAINT human_action_action_id_foreign FOREIGN KEY (action_id) REFERENCES Action (id);
