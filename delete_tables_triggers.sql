a621aeb48c6681e441f3e7ee966fae9078fb8351
Vadim Ponomarev <38704817+VxDxK@users.noreply.github.com>
2024-01-14 11:26:42 -0800

    Rewrite ddl.sql, add part of dml-generator (#1)
    * Rewrite ddl.sql, add part of dml-generator
    
    * Я заебался, поэтому последние генераторы некрасивые
    
    * Биба боба пизда чулочек

--- delete_tables_triggers.sql
+++ delete_tables_triggers.sql
@@ -1,26 +1,25 @@
-drop trigger if exists before_insert_sacrifice on sacrifice;
-drop trigger if exists before_insert_outsider_1 on outsider;
-drop trigger if exists before_insert_outsider_2 on outsider;
-drop trigger if exists before_insert_outsider_3 on outsider;
-
-drop table if exists sacrifice;
-drop table if exists altar;
-drop table if exists elected_ability;
-drop table if exists ability;
-drop table if exists elected;
-drop table if exists instrument;
-drop table if exists instrument_status;
-drop table if exists runes;
-drop table if exists human_action;
-drop table if exists action;
-drop table if exists outsider;
-drop table if exists abbys;
-drop table if exists abbey;
-drop table if exists abbey_member;
-drop table if exists abbey_rank_status;
-drop table if exists human;
-drop table if exists human_status;
+-- drop trigger if exists before_insert_sacrifice on sacrifice;
+-- drop trigger if exists before_insert_outsider_1 on outsider;
+-- drop trigger if exists before_insert_outsider_2 on outsider;
+-- drop trigger if exists before_insert_outsider_3 on outsider;
 
 
+DROP TABLE IF EXISTS db_cs_human_action;
+DROP TABLE IF EXISTS db_cs_action;
+DROP TABLE IF EXISTS db_cs_elected_ability;
+DROP TABLE IF EXISTS db_cs_sacrifice;
+DROP TABLE IF EXISTS db_cs_abbey_rank_status;
+DROP TABLE IF EXISTS db_cs_rune;
+DROP TABLE IF EXISTS db_cs_elected;
+DROP TABLE IF EXISTS db_cs_instrument;
+DROP TABLE IF EXISTS db_cs_altar;
+DROP TABLE IF EXISTS db_cs_abbey_member;
+DROP TABLE IF EXISTS db_cs_abbey;
+DROP TABLE IF EXISTS db_cs_ability;
+DROP TABLE IF EXISTS db_cs_outsider;
+DROP TABLE IF EXISTS db_cs_abbys;
+DROP TABLE IF EXISTS db_cs_human;
 
-
+DROP TYPE IF EXISTS db_cs_human_status;
+DROP TYPE IF EXISTS db_cs_instrument_status;
+DROP TYPE IF EXISTS db_cs_abbey_member_rank;
